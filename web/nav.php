0f15892d37d9cf1c7f6ebf7c194274cb6e284b96
liangfenglw <liangfenglw@qq.com>
2017-02-18 18:13:57 +0800

    nav 底部导航修改

--- web/nav.php
+++ web/nav.php
@@ -3,18 +3,23 @@
 .clr{	clear:both;	}
 #f_nav {	background:#000;	text-align:center;	padding:10px 0;	text-align:center;	line-height:1.8;	}
 #f_nav a{	color:#fff;		padding:0px 10px;	}
+#f_nav strong{	color:#E53935;	}
 </style>
 
 <div class="clr" id="f_nav">
 	<a href="index.php">首页</a>
-	<a href="login.php">登录</a>
-	<a href="reg.php">注册</a>
-	<a href="fapiao.php">发票申请</a>
-	<a href="chongzhi.php">账户充值</a>
-	<a href="huiyuanedit.php">会员资料编辑</a>
-	<a href="baike.php">网络-百科</a>
+	<a href="fapiao.php" title="申请发票.psd">申请发票</a>
+	
+	<br/>
+	<strong>会员中心</strong>
+	<a href="login.php" title="会员中心/登录.psd">登录</a>
+	<a href="reg.php" title="会员中心/注册.psd">注册</a>
+	<a href="chongzhi.php" title="会员中心/帐户充值.psd">账户充值</a>
+	<a href="huiyuanedit.php" title="会员中心/会员中心.psd">会员中心</a>
 
 	<br/>
+	<strong>网络媒体：</strong>
+	<a href="baike.php" title="网络媒体/百科.psd">百科</a>
 	<a href="shortvideo.php" title="网络媒体/短视频.psd">短视频</a>
 	<a href="fabugongzhonghao.php" title="网络媒体/发布公众号任务.psd">发布公众号任务</a>
 	<a href="bbs.php" title="网络媒体/论坛.psd">论坛</a>
@@ -28,7 +33,8 @@
 	<a href="newstask.php" title="网络媒体/新闻任务.psd">新闻任务</a>
 	
 	<br/>
-	<a href="huodongorder.php" title="媒体供应商_活动订单.psd">活动订单</a>
+	<strong>媒体供应商：</strong>
+	<a href="huodongorder.php" title="媒体供应商_活动订单.psd">媒体供应商_活动订单</a>
 	
 	<br/>
 	<a href="fenlei.php" title="新增页面-筛选分类管理.psd">分类管理</a>
